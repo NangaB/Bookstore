740ca11219facbe6f809894de6b78d712088dd34
Beata <operchalska.beata@gmail.com>
2022-11-17 19:35:12 +0100

    Initialization

---
+++ src/main/java/com/app/bobbookstore/BobBookstoreApplication.java
@@ -0,0 +1,13 @@
+package com.app.bobbookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BobBookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BobBookstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/bobbookstore/BobBookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.bobbookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BobBookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
